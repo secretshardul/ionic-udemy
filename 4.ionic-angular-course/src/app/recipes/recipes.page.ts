bf69ae8c257770ccab0eda66c221bbbc3d695ed3
Shardul Aeer <shardulaeer@HP.local>
2020-03-19 13:29:30 +0530

    4.4: Read recipes from service

--- 4.ionic-angular-course/src/app/recipes/recipes.page.ts
+++ 4.ionic-angular-course/src/app/recipes/recipes.page.ts
@@ -1,30 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipe.model';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "./recipe.model";
+import { RecipesService } from "./recipes.service";
 
 @Component({
-  selector: 'app-recipes',
-  templateUrl: './recipes.page.html',
-  styleUrls: ['./recipes.page.scss'],
+  selector: "app-recipes",
+  templateUrl: "./recipes.page.html",
+  styleUrls: ["./recipes.page.scss"]
 })
 export class RecipesPage implements OnInit {
-  recipes: Recipe[] = [
-    {
-      id: 'r1',
-      title: 'Burger',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/800px-RedDot_Burger.jpg',
-      ingredients: ['Bread', 'Cheese', 'Patty']
-    },
-    {
-      id: 'r2',
-      title: 'Pizza',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Pizza-3007395.jpg/800px-Pizza-3007395.jpg',
-      ingredients: ['Dough', 'Cheese', 'Tomato']
-    }
-  ]
-
-  constructor() { }
+  recipes: Recipe[];
+  constructor(private recipesService: RecipesService) {} //intercept service
 
   ngOnInit() {
+    this.recipes = this.recipesService.getAllRecipes();
   }
-
 }
---
+++ 4.ionic-angular-course/src/app/recipes/recipes.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@angular/core";
+import { Recipe } from "./recipe.model";
+
+@Injectable({
+  providedIn: "root"
+})
+export class RecipesService {
+  private recipes: Recipe[] = [
+    {
+      id: "r1",
+      title: "Burger",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/800px-RedDot_Burger.jpg",
+      ingredients: ["Bread", "Cheese", "Patty"]
+    },
+    {
+      id: "r2",
+      title: "Pizza",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Pizza-3007395.jpg/800px-Pizza-3007395.jpg",
+      ingredients: ["Dough", "Cheese", "Tomato"]
+    }
+  ];
+
+  constructor() {}
+
+  getAllRecipes(): Recipe[] {
+    return [...this.recipes]; //pass complete array not reference
+  }
+  getRecipe(recipeId: string) {
+    const foundRecipe = this.recipes.find(recipe => {
+      return recipe.id == recipeId;
+    });
+    return { ...foundRecipe }; //pass complete object not reference
+  }
+}
