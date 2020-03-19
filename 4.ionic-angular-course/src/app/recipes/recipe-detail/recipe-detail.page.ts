dd68e3dbf25227540157393709265005e043d3be
Shardul Aeer <shardulaeer@HP.local>
2020-03-19 15:43:51 +0530

    4.5: recipe-detail page - Use 'ActivatedRoute' observable to find current route - Fetch recipe from service

--- 4.ionic-angular-course/src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ 4.ionic-angular-course/src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { RecipesService } from "../recipes.service";
+import { Recipe } from '../recipe.model';
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.page.html',
-  styleUrls: ['./recipe-detail.page.scss'],
+  selector: "app-recipe-detail",
+  templateUrl: "./recipe-detail.page.html",
+  styleUrls: ["./recipe-detail.page.scss"]
 })
 export class RecipeDetailPage implements OnInit {
-
-  constructor() { }
+  loadedRecipe: Recipe;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private recipesService: RecipesService
+  ) {}
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramMap => {//observer
+      if (!paramMap.has("recipeId")) {
+        return;
+      }
+      const recipeId = paramMap.get("recipeId");
+      this.loadedRecipe = this.recipesService.getRecipe(recipeId);
+      console.log(this.loadedRecipe.title);
+    });
   }
-
 }
